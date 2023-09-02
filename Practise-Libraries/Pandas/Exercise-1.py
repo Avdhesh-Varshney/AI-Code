9191c50c7b678c2079a55254d29a1a5c3f033162
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-09-02 13:03:30 +0530

    Day-1
    Pandas practice problems - 1st Day Completed!

--- Practise-Libraries/Pandas/Exercise-1.py
+++
@@ -1,9 +0,0 @@
-# create a Series object and print it to the console
-
-import pandas as pd
-
-stocks = ['PLW', 'CDR', '11B', 'TEN']
-
-# <---- Write your code here ---->
-
-print(pd.Series(data=stocks))

c1e697b318e9bb3d6574eec7ee5090510570021c
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2023-09-05 17:31:13 +0530

    Day-3
    Numpy and Pandas problems - Day 3 Completed!

--- Practise-Libraries/Numpy/Exercise-11.py
+++
@@ -1,7 +0,0 @@
-# Using Numpy create a one-dimensional array of all two-digit numbers and print this array to the console.
-
-import numpy as np
-
-# <---- Write your code here ---->
-
-print(np.arange(10, 100))
--- Practise-Libraries/Numpy/Exercise-15.py
+++
@@ -1,9 +0,0 @@
-# Using Numpy create a two-dimensional array with the shape (10, 4) pseudo-randomly generated values from the standard normal distribution N(0,1). Set the random seed to 20. Print result to the console as shown below.
-
-import numpy as np
-
-# <---- Write your code here ---->
-
-np.random.seed(20)
-
-print(np.random.randn(10, 4))

97ff52d5e4de45d19d2b2624ad496d2fbb83ba95
Lycons-rage <chinmayatewari.20@gmail.com>
2024-06-22 00:46:39 +0530

    Algorithms Standard_Scaler, Min_Max_Scaler, Ordinal_Encoder added, also respective documentation added

---
+++ Pre-Processing/Algorithms/Ordinal_Encoder/ordinal_encoder.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+class OrdinalEncoding:
+    def __init__(self):
+        self.category_mapping = {}
+    
+    def fit(self, data):
+        # Fit the encoder to the data (pandas DataFrame).
+        # type check
+        if not type(data)==pd.DataFrame:
+            raise f"Type of data should be Pandas.DataFrame; {type(data)} found"
+        for column in data.columns:
+            unique_categories = sorted(set(data[column]))
+        self.category_mapping = {category: idx for idx, category in enumerate(unique_categories)}
+    
+    def transform(self, data):
+        # Transform the data (pandas DataFrame) to ordinal integers.
+        # checking for empty mapping
+        if not self.category_mapping:
+            raise "Catrgorical Mapping not found. Call OrdinalExcoding.fit() method or call OrdinalEncoding.fit_transform() method"
+        
+        data_transformed = data.copy()
+        for column in data.columns:
+            data_transformed[column] = data[column].map(self.category_mapping[column])
+        return data_transformed
+    
+    def fit_transform(self, data):
+        # Fit the encoder and transform the data in one step.
+        self.fit(data)
+        return self.transform(data)
